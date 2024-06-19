1211484483879ac92a0317df8096e7ddadc433ab
Les Carbonaro <lcarbonaro@gmail.com>
2024-06-19 09:06:18 -0400

    try fix views path

--- api/index.js
+++ api/index.js
@@ -1,11 +1,16 @@
 const express = require("express");
+const path = require("path");
+
 const app = express();
 
 app.set('view engine', 'ejs'); // Set EJS as the view engine
 
-app.set('views', 'views'); // Specify the directory where EJS templates are located
+//app.set('views', 'views'); // Specify the directory where EJS templates are located
 //app.set("views", __dirname + "/views");
 //app.use(express.static(__dirname + "views"));
+app.set("views", path.join(__dirname, "views"));
+
+
 
 
 app.get("/", (req, res) => res.send("Express on Vercel"));
