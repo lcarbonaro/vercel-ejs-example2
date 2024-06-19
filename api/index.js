cf01153f31990591ebb8dc052f8f10657921a830
Les Carbonaro <lcarbonaro@gmail.com>
2024-06-19 08:33:22 -0400

    add index.ejs

--- api/index.js
+++ api/index.js
@@ -1,7 +1,14 @@
 const express = require("express");
 const app = express();
 
+app.set('view engine', 'ejs'); // Set EJS as the view engine
+app.set('views', './views'); // Specify the directory where EJS templates are located
+
 app.get("/", (req, res) => res.send("Express on Vercel"));
+
+app.get("/hello", (req, res) => {
+    res.render('index');
+});
 
 app.listen(3000, () => console.log("Server ready on port 3000."));
 
